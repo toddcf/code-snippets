3c40caa721c45f704c6d2de75bda2514bd98305a
Todd Croak-Falen <toddcf@gmail.com>
2017-03-02 17:49:17 -0800

    Added JavaScript keypress.

---
+++ javascript/keypress.js
@@ -0,0 +1,6 @@
+// Event listener for ENTER key:
+document.addEventListener("keypress", function(event) {
+	if ( event.keyCode === 13 || event.which === 13 ) {
+		console.log("ENTER key was pressed!");
+	}
+});
