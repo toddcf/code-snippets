4cbfa89db05bdee1213636759a34281f2759a901
Todd Croak-Falen <toddcf@gmail.com>
2017-03-02 18:18:02 -0800

    Added switch statement snippet.

--- javascript/decisions/ternary-operator.js
+++ javascript/decisions/ternary-operator.js
@@ -4,4 +4,4 @@ activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
 /*
 In plain English:
 - IF activePlayer strictly equals zero, THEN set activePlayer to 1.
-- OTHERWISE, set activePlayer to zero.
+- ELSE, set activePlayer to zero.
