ad5b64c2acaff8a7f4e592511be142b0d7df18be
Todd Croak-Falen <toddcf@gmail.com>
2017-03-02 18:11:56 -0800

    Added ternary operator snippet.

---
+++ javascript/if-else/ternary-operator.js
@@ -0,0 +1,7 @@
+// Ternary Operator:
+activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+
+/*
+In plain English:
+- IF activePlayer strictly equals zero, THEN set activePlayer to 1.
+- OTHERWISE, set activePlayer to zero.
